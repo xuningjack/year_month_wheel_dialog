3386def27560a36dfd9715e73adf471d37e08f55
xuning108986@pwrd.com <xuning108986@pwrd.com>
2017-02-28 11:41:39 +0800

    delete gen and bin

--- gen/com/example/android_year_month_wheel/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.example.android_year_month_wheel;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
